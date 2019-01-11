3563c61f3273e8b72a13a9d5758eccb457438e93
MrHplan <771631419@qq.com>
2019-01-11 14:44:00 +0800

    bean

---
+++ src/com/study/bean/Collect.java
@@ -0,0 +1,94 @@
+package com.study.bean;
+
+public class Collect {
+
+	private int id;
+	private int user_id;
+	private int item_id;
+	private String title;
+	private double price;
+	private String pic_path;
+	private String item_param_data;
+	private int status;
+	private String created;
+	private String updated;
+	public Collect()
+	{
+		
+	}
+	public Collect(int id, int user_id, int item_id, String title, double price, String pic_path,
+			String item_param_data, int status, String created, String updated) {
+		super();
+		this.id = id;
+		this.user_id = user_id;
+		this.item_id = item_id;
+		this.title = title;
+		this.price = price;
+		this.pic_path = pic_path;
+		this.item_param_data = item_param_data;
+		this.status = status;
+		this.created = created;
+		this.updated = updated;
+	}
+	public int getId() {
+		return id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public int getItem_id() {
+		return item_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public String getPic_path() {
+		return pic_path;
+	}
+	public String getItem_param_data() {
+		return item_param_data;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public String getUpdated() {
+		return updated;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public void setItem_id(int item_id) {
+		this.item_id = item_id;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public void setPic_path(String pic_path) {
+		this.pic_path = pic_path;
+	}
+	public void setItem_param_data(String item_param_data) {
+		this.item_param_data = item_param_data;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public void setUpdated(String updated) {
+		this.updated = updated;
+	}
+	
+}
---
+++ src/com/study/bean/Item.java
@@ -0,0 +1,119 @@
+package com.study.bean;
+
+public class Item {
+	private int id;
+	private int cid;
+	private String brand;
+	private String model;
+	private String title;
+	private String sell_point;
+	private int price;
+	private int num;
+	private String barcode;
+	private String image;
+	private int status;
+	private String created;
+	private String updated;
+	
+	public Item() {
+		super();
+	}
+	
+	public Item(int id, int cid, String brand, String model, String title, String sell_point, int price, int num,
+			String barcode, String image, int status, String created, String updated) {
+		super();
+		this.id = id;
+		this.cid = cid;
+		this.brand = brand;
+		this.model = model;
+		this.title = title;
+		this.sell_point = sell_point;
+		this.price = price;
+		this.num = num;
+		this.barcode = barcode;
+		this.image = image;
+		this.status = status;
+		this.created = created;
+		this.updated = updated;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getSell_point() {
+		return sell_point;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getNum() {
+		return num;
+	}
+	public String getBarcode() {
+		return barcode;
+	}
+	public String getImage() {
+		return image;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public String getUpdated() {
+		return updated;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setSell_point(String sell_point) {
+		this.sell_point = sell_point;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public void setUpdated(String updated) {
+		this.updated = updated;
+	}
+	
+}
